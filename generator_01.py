01a1e3aeb592f0c7d5a2acba9d918ef3919b2305
Yeobin Yun <duqls456@gmail.com>
2025-07-28 15:29:31 +0900

    add generator functions to create and print number sequences

---
+++ generator_01.py
@@ -0,0 +1,19 @@
+def generate_numbers_1(max_number: int):
+    print("called generate_numbers_1 function")
+    numbers = []
+    for i in range(1, max_number+1):
+        numbers.append(i)
+    return numbers
+
+#Generator 객체를 만드는 함수 - yiled를 사용
+def generate_numbers_2(max_number: int):
+    print("called generate_numbers_2 function")
+    for i in range(1, max_number+1):
+        yield i
+
+
+
+numbers_1 = generate_numbers_1(10)
+numbers_2 = generate_numbers_2(10)
+print(numbers_1)
+print(numbers_2)
